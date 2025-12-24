467c55768678fd39e06b7065abbcb5a0f8844ca6
Junnwan <danielwan189@gmail.com>
2025-12-24 17:14:29 +0800

    amended SecurityHub

--- terraform/modules/securityhub/main.tf
+++ terraform/modules/securityhub/main.tf
@@ -5,7 +5,4 @@ resource "aws_securityhub_standards_subscription" "cis" {
   standards_arn = "arn:aws:securityhub:::ruleset/cis-aws-foundations-benchmark/v/1.2.0"
 }
 
-resource "aws_securityhub_standards_subscription" "pci" {
-  depends_on    = [aws_securityhub_account.main]
-  standards_arn = "arn:aws:securityhub:us-east-1::standards/pci-dss/v/3.2.1"
-}
+
